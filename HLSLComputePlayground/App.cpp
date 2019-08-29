5c9e88c89e486efec8a53e556f221ed6b85e2bc0
S Litherum <litherum@icloud.com>
2019-08-28 23:34:49 -0700

    Add GLSL compute playground

--- HLSLComputePlayground/App.cpp
+++ HLSLComputePlayground/App.cpp
@@ -76,9 +76,9 @@ static winrt::Windows::Foundation::IAsyncAction run() {
 	com_ptr<ID3D12PipelineState> pipelineState;
 	check_hresult(device->CreateComputePipelineState(&pipelineStateDescriptor, __uuidof(pipelineState), pipelineState.put_void()));
 
-	float rawBuffer1Data[] = { -0.0, 0.0 };
+	float rawBuffer1Data[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
 	constexpr int numFloatsUpload = _countof(rawBuffer1Data);
-	constexpr int numFloatsReadback = 1;
+	constexpr int numFloatsReadback = 16;
 
 	auto defaultHeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
 	auto uploadHeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
