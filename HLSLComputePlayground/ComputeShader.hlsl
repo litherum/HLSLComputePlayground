8b65add43e983a0e022e2a9d6ef2e6cc1a87f4d2
Litherum <litherum@icloud.com>
2019-08-29 13:40:37 -0700

    HLSL matrix multiplication

--- HLSLComputePlayground/ComputeShader.hlsl
+++ HLSLComputePlayground/ComputeShader.hlsl
@@ -1,8 +1,8 @@
-RWStructuredBuffer<float> buffer1 : register(u0);
-RWStructuredBuffer<float> buffer2 : register(u1);
+RWStructuredBuffer<float4x4> buffer1 : register(u0);
+RWStructuredBuffer<float4x4> buffer2 : register(u1);
 
 [numthreads(1, 1, 1)]
 void main()
 {
-	buffer2[0] = ldexp(2.5, 3.5);
+	buffer2[0] = mul(buffer1[0], buffer1[1]);
 }
